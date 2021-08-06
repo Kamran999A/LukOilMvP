7f146fe3aa6de34a14cf84405529d0d56d2d7673
Ruslan <ruslanmustafayevaydin2000@gmail.com>
2021-08-06 09:50:32 +0400

    Add project files.

---
+++ Data/PetrolOperationContext.cs
@@ -0,0 +1,21 @@
+﻿using LukoilMVP.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LukoilMVP.Data
+{
+    public class PetrolOperationContext : DbContext
+    {
+        public PetrolOperationContext()
+            :base("PetrolOperationDB")
+        {
+        }
+
+        public DbSet<PetrolPaymentOperation> PetrolPaymentOperations { get; set; }
+
+    }
+}
---
+++ Models/PetrolPaymentOperation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LukoilMVP.Models
+{
+    public class PetrolPaymentOperation
+    {
+        public int PetrolPaymentOperationID { get; set; }
+        public Petrol Petrol { get; set; }
+        public bool IsBoughtLitr { get; set; }
+        public decimal BoughtPrice { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        public override string ToString()
+        {
+            string state = IsBoughtLitr ? "Bought with litr" : "Bought with price";
+            return $"Name: {Petrol.Name}, Initial Price: {Petrol.InitialPrice:C}, {state}, Bought Price: {BoughtPrice:C}, Date: {Date}";
+        }
+
+    }
+}
